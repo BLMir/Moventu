fad5f81e17d150608dcb1956f294f31b9e5d50c4
biel <biel.mir@trivago.com>
2019-04-21 20:14:22 +0200

    logging system added

--- src/main/kotlin/com/moventum/randomrouter/main.kt
+++ src/main/kotlin/com/moventum/randomrouter/main.kt
@@ -1,19 +1,24 @@
 package com.moventum.randomrouter
 
+import com.moventum.randomrouter.service.PerimeterService
 import com.moventum.randomrouter.service.RouteService
 import com.moventum.randomrouter.service.RandomRouterServiceGrpcImpl
 import config.config
 import config.serverPort
 import io.grpc.ServerBuilder
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger{}
 
 fun main(args: Array<String>) {
-
-    val generateRouteService = RouteService()
+    val perimeterService = PerimeterService()
+    val generateRouteService = RouteService(perimeterService)
     val server = ServerBuilder
         .forPort(config[serverPort])
         .addService(RandomRouterServiceGrpcImpl(generateRouteService))
         .build()
 
+    logger.info { "Grpc endpoint listen port ${config[serverPort]}" }
     server.start()
     server.awaitTermination()
 }
