af08df90f422c2121f43544d121aff175ebbfbf3
biel <biel.mir@trivago.com>
2019-04-21 12:57:17 +0200

    change the float to double to remove redundancy

--- src/main/kotlin/com/moventum/randomrouter/setup.kt
+++ src/main/kotlin/com/moventum/randomrouter/setup.kt
@@ -1,5 +1,9 @@
 package com.moventum.randomrouter
 
+import com.moventum.randomrouter.component.toMeters
+
 const val AVG_WALK_SPEED = 10
 const val MIN_WAY_POINTS = 5
 const val MAX_WAY_POINTS = 10
+
+const val EARTH_RADIUS = 6371
