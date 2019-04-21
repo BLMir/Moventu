417529eb2b8e41730c73223432031a8089b99fb0
biel <biel.mir@trivago.com>
2019-04-21 20:14:22 +0200

    logging system added

--- src/main/kotlin/com/moventum/randomrouter/service/RandomRouterServiceGrpcImpl.kt
+++ src/main/kotlin/com/moventum/randomrouter/service/RandomRouterServiceGrpcImpl.kt
@@ -4,9 +4,13 @@ import com.moventum.randomrouter.RandomRouterGrpc
 import com.moventum.randomrouter.Route
 import com.moventum.randomrouter.RouteReq
 import io.grpc.stub.StreamObserver
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger{}
 
 class RandomRouterServiceGrpcImpl(private val routeService: RouteService): RandomRouterGrpc.RandomRouterImplBase() {
     override fun route(request: RouteReq, responseObserver: StreamObserver<Route>) {
+        logger.info { "New route has been requested: $request" }
         responseObserver.onNext(routeService.getRoute(request))
         responseObserver.onCompleted()
     }
--- src/main/kotlin/com/moventum/randomrouter/service/RouteService.kt
+++ src/main/kotlin/com/moventum/randomrouter/service/RouteService.kt
@@ -3,11 +3,11 @@ package com.moventum.randomrouter.service
 import com.moventum.randomrouter.Route
 import com.moventum.randomrouter.RouteReq
 
-class RouteService {
+class RouteService(private val perimeterService: PerimeterService) {
     fun getRoute(route: RouteReq) : Route{
 
-        val generatePerimeterService = PerimeterService()
-        generatePerimeterService.createPerimeter(route, 2.0)
+
+        perimeterService.createPerimeter(route, 2.0)
         return Route.newBuilder().setRoute("here").build()
     }
 }
