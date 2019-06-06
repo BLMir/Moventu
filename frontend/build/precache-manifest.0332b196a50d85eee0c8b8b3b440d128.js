88a275781069866553f18564fc2103cf7f26cd78
Lewis <billobera@gmail.com>
2019-06-06 20:48:29 +0200

    build

---
+++ frontend/build/precache-manifest.0332b196a50d85eee0c8b8b3b440d128.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d375aa3d832b41df6820",
+    "url": "/static/js/main.e302ca53.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "df18640829aa980a3842",
+    "url": "/static/js/2.fb53b6e8.chunk.js"
+  },
+  {
+    "revision": "dc473caad6371067cc152360b1e5d474",
+    "url": "/static/media/testmap.dc473caa.png"
+  },
+  {
+    "revision": "f39584e6737882889c17c9902272042d",
+    "url": "/index.html"
+  }
+];
