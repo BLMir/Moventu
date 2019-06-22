b8432275a0d937240c1766bf7ecdb5dfee501a05
Lewis <billobera@gmail.com>
2019-06-22 12:26:23 +0200

    convert function to arrow

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -3,20 +3,18 @@ import Wrapper from '../elements/Wrapper';
 import PageTitle from '../elements/PageTitle';
 import HeaderBack from '../components/HeaderBack';
 
-function About() {
-  return (
-    <div>
-      <HeaderBack />
-      <Wrapper between>
-        <PageTitle>What dis?</PageTitle>
-        <p>
-          Moventu is born from the necessity to encourage people to GO OUT,
-          making them feel the motivation to discover new locations, with
-          indications of random and personal Routes.
-        </p>
-      </Wrapper>
-    </div>
-  );
-}
+const About = () => (
+  <div>
+    <HeaderBack />
+    <Wrapper between>
+      <PageTitle>What dis?</PageTitle>
+      <p>
+        Moventu is born from the necessity to encourage people to GO OUT, making
+        them feel the motivation to discover new locations, with indications of
+        random and personal Routes.
+      </p>
+    </Wrapper>
+  </div>
+);
 
 export default About;
