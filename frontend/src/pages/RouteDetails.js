3da196b8606a2cd568baa686223da37c7e005b33
Lewis <billobera@gmail.com>
2019-06-22 12:28:03 +0200

    reorder imports

--- frontend/src/pages/RouteDetails.js
+++ frontend/src/pages/RouteDetails.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 import Wrapper from '../elements/Wrapper';
 import PageTitle from '../elements/PageTitle';
