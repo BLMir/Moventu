fbe6a78450080259d8caf5931df2c87f7ef0bdaf
Lewis <billobera@gmail.com>
2019-06-24 15:27:39 +0200

    add navigation

--- frontend-rn/App.js
+++ frontend-rn/App.js
@@ -1,25 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-import { AppLoading, Asset } from 'expo';
 
 import Navigation from './navigation';
-import { Block } from './components';
-import * as constants from './constants';
+import * as constants from './constants'
+
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Navigation />
-    </View>
-  );
+  return <Navigation />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'hsl(0, 0%, 5%)',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-});
