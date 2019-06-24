9536aba3f6fb8290b59148a8a69e8b6bf4ea2366
Lewis <billobera@gmail.com>
2019-06-24 17:03:31 +0200

    add theme

---
+++ frontend-rn/constants/theme.js
@@ -0,0 +1,11 @@
+const color = {
+  dark: 'hsl(0, 0%, 10%)',
+  darker: 'hsl(0, 0%, 5%)',
+  light: 'hsl(0, 0%, 90%)',
+  lighter: 'hsl(0, 0%, 95%)',
+  accent: 'hsl(110, 79%, 50%)'
+}
+
+const size = {
+
+}
