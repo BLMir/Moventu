8fa1953965168ebdfce183e861804d860309b5f5
Lewis <billobera@gmail.com>
2019-06-25 23:05:14 +0200

    renamed component

---
+++ frontend-rn/components/Container.js
@@ -0,0 +1,12 @@
+import { View } from 'react-native';
+import styled from 'styled-components';
+
+import { theme } from '../constants';
+
+export default styled.View`
+  display: flex;
+  flex-grow: 1;
+  background-color: ${theme.color.dark};
+  align-items: center;
+  justify-content: center;
+`;
