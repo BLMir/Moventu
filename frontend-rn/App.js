86defd9ed4c5943ce933d8cf532d2eb7f4a7a8a7
Lewis <billobera@gmail.com>
2019-06-26 09:59:00 +0200

    add navigation

--- frontend-rn/App.js
+++ frontend-rn/App.js
@@ -1,6 +1,27 @@
 import React from 'react';
+import { Image } from 'react-native';
+import { createAppContainer, createStackNavigator } from 'react-navigation';
 
-import Navigation from './navigation';
+import Welcome from './screens/Welcome';
+import SetRoute from './screens/SetRoute';
+
+const screens = createStackNavigator(
+  {
+    Welcome,
+    SetRoute
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {},
+      headerBackImage: <Image />,
+      headerBackTitle: null,
+      headerLeftContainerStyle: {},
+      headerRightContainerStyle: {}
+    }
+  }
+);
+
+const Navigation = createAppContainer(screens);
 
 export default function App() {
   return <Navigation />;
