9a6b00309d7a9fa2c1e17e0521887c52e4ed79bd
Lewis <billobera@gmail.com>
2019-06-26 09:31:17 +0200

    add screen

---
+++ frontend-rn/screens/SetRoute.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { theme } from '../constants';
+import Container from '../components/Container';
+import Heading from '../components/Heading';
+import Button from '../components/Button';
+import ButtonText from '../components/StyledText';
+
+const SetRoute = () => (
+  <Container>
+    <Heading>Set Route</Heading>
+    <Button primary onPress={() => {}}>
+      <ButtonText large bold>
+        START
+      </ButtonText>
+    </Button>
+  </Container>
+);
+
+SetRoute.navigationOptions = {
+  header: null
+};
+
+export default SetRoute;
