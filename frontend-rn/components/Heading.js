af962eb2a804650073a7b27571157defef127d42
Lewis <billobera@gmail.com>
2019-06-26 11:11:44 +0200

    edit style

--- frontend-rn/components/Heading.js
+++ frontend-rn/components/Heading.js
@@ -5,6 +5,8 @@ import { theme } from '../constants';
 
 export default styled.Text`
   color: ${theme.color.lighter};
-  font-size: ${props => (props.large ? theme.size.l : theme.size.m)};
+  font-size: ${props => (props.large ? theme.size.xl : theme.size.m)};
   font-weight: bold;
+  text-align: left;
+  margin-bottom: ${theme.space.m};
 `;
