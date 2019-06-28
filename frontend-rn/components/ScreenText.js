f15a44fa67fceb66a16d8d6930cec339facfb59f
Lewis <billobera@gmail.com>
2019-06-28 18:28:51 +0200

    add padding

--- frontend-rn/components/ScreenText.js
+++ frontend-rn/components/ScreenText.js
@@ -7,4 +7,8 @@ export default styled.Text`
   color: ${theme.color.light};
   font-size: ${props => (props.large ? theme.size.m : theme.size.s)};
   font-weight: ${props => (props.bold ? 'bold' : 'normal')};
+  padding-top: ${theme.space.xs};
+  padding-bottom: ${theme.space.xs};
+  padding-right: ${theme.space.xs};
+  padding-left: ${theme.space.xs};
 `;
