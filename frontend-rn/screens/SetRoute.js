4daca26487059a3eff9953790024238cd3572d89
Lewis <billobera@gmail.com>
2019-06-28 17:46:10 +0200

    edit styles

--- frontend-rn/screens/SetRoute.js
+++ frontend-rn/screens/SetRoute.js
@@ -1,35 +1,39 @@
 import React from 'react';
-import { TextInput, KeyboardAvoidingView } from 'react-native';
+import { TextInput, KeyboardAvoidingView, Platform } from 'react-native';
 import styled from 'styled-components';
 
 import { theme } from '../constants';
 
 import Container from '../components/Container';
-import Heading from '../components/Heading';
+import ScreenHeading from '../components/ScreenHeading';
 import ScreenText from '../components/ScreenText';
 import Button from '../components/Button';
 import ButtonText from '../components/ButtonText';
 
 const InputDistance = styled.TextInput`
   height: 50;
-  width: 300;
+  width: 200;
   text-align: center;
   background-color: ${theme.color.light};
   color: ${theme.color.dark};
   border-radius: ${theme.radius.normal};
+  margin-top: ${theme.space.s};
   margin-bottom: ${theme.space.m};
-`;
-
-const StyledHeading = styled(Heading)`
-  margin-bottom: 10;
+  font-size: ${theme.size.m};
+  margin-left: auto;
+  margin-right: auto;
 `;
 
 const SetRoute = ({ navigation }) => (
   <Container>
-    <Heading large>Set Route</Heading>
-    <StyledHeading>How far do you want to go?</StyledHeading>
-    <ScreenText />
-    <KeyboardAvoidingView behavior="padding" enabled>
+    <ScreenHeading>Set Route</ScreenHeading>
+    <ScreenText large bold>
+      How many km?
+    </ScreenText>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : null}
+      enabled
+    >
       <InputDistance keyboardType={'numeric'} />
       <Button primary onPress={() => navigation.navigate('MapRoute')}>
         <ButtonText large bold>
