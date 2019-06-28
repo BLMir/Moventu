d935fcc017f0b816c7edeaca0c2424341c2ba9c1
Lewis <billobera@gmail.com>
2019-06-28 18:28:22 +0200

    edit layout

--- frontend-rn/components/Container.js
+++ frontend-rn/components/Container.js
@@ -8,6 +8,6 @@ export default styled.View`
   flex-grow: 1;
   background-color: ${theme.color.dark};
   align-items: center;
-  justify-content: center;
-  padding-top: ${theme.space.s};
+  justify-content: flex-start;
+  padding-top: ${theme.space.xl};
 `;
