70f3af9f3054ccc3d68256004a5f773a96250dfd
Lewis <billobera@gmail.com>
2019-06-28 18:57:04 +0200

    edit text

--- frontend-rn/screens/About.js
+++ frontend-rn/screens/About.js
@@ -11,11 +11,11 @@ const About = ({ navigation }) => (
   <Container>
     <ScreenHeading>About</ScreenHeading>
     <ScreenText>
-      Moventu is an app born from the necessity to encourage
-      users to GO OUT, making them feel the motivation to explore and discover
-      new locations around them.
-      {"\n"}
-      {"\n"}
+      Moventu is an app born from the necessity to encourage users to GO OUT,
+      making them feel the motivation to explore and discover new locations
+      around them.
+      {'\n'}
+      {'\n'}
       It serves a random loop route based on location and desired activity
       distance, and guides the user one step at a time, without revealing the
       full route or directions.
