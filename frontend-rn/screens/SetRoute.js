42edf21cc13f6ade5dec7df69eb5b1465cbdd9fe
Lewis <billobera@gmail.com>
2019-06-30 01:18:16 +0200

    swap text input for slider

--- frontend-rn/screens/SetRoute.js
+++ frontend-rn/screens/SetRoute.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { TextInput, KeyboardAvoidingView, Platform } from 'react-native';
-import styled from 'styled-components';
+import React, { useState } from 'react';
+import { KeyboardAvoidingView, Platform } from 'react-native';
+import Slider from 'react-native-slider';
 
 import { theme } from '../constants';
 
@@ -10,39 +10,34 @@ import ScreenText from '../components/ScreenText';
 import Button from '../components/Button';
 import ButtonText from '../components/ButtonText';
 
-const InputDistance = styled.TextInput`
-  height: 50;
-  width: 200;
-  text-align: center;
-  background-color: ${theme.color.light};
-  color: ${theme.color.dark};
-  border-radius: ${theme.radius.normal};
-  margin-top: ${theme.space.s};
-  margin-bottom: ${theme.space.m};
-  font-size: ${theme.size.m};
-  margin-left: auto;
-  margin-right: auto;
-`;
+const SetRoute = ({ navigation }) => {
+  const [distance, setDistance] = useState(0);
 
-const SetRoute = ({ navigation }) => (
-  <Container>
-    <ScreenHeading>Set Route</ScreenHeading>
-    <ScreenText large bold>
-      How many km?
-    </ScreenText>
-    <KeyboardAvoidingView
-      behavior={Platform.OS === 'ios' ? 'padding' : null}
-      enabled
-    >
-      <InputDistance keyboardType={'numeric'} />
-      <Button primary onPress={() => navigation.navigate('MapRoute')}>
-        <ButtonText large bold>
-          START
-        </ButtonText>
-      </Button>
-    </KeyboardAvoidingView>
-  </Container>
-);
+  return (
+    <Container>
+      <ScreenHeading>Set Route</ScreenHeading>
+      <ScreenText large bold>
+        How many km?
+      </ScreenText>
+      <ScreenText>{distance}</ScreenText>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : null}
+        enabled
+      >
+        <Slider
+          style={{ marginBottom: theme.space.m }}
+          value={0}
+          onValueChange={() => setDistance(distance)}
+        />
+        <Button primary onPress={() => navigation.navigate('MapRoute')}>
+          <ButtonText large bold>
+            START
+          </ButtonText>
+        </Button>
+      </KeyboardAvoidingView>
+    </Container>
+  );
+};
 
 SetRoute.navigationOptions = {
   header: null
