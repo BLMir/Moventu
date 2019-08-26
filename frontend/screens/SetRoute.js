f09e76279986cecc3fb513ce9abe643fd4cb263d
Lewis Llobera <billobera@gmail.com>
2019-08-26 11:48:59 +0200

    Fix permission error

--- frontend/screens/SetRoute.js
+++ frontend/screens/SetRoute.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { SafeAreaView } from 'react-native';
+import { Alert, SafeAreaView } from 'react-native';
 import Geolocation from 'react-native-geolocation-service';
 
 import { theme } from '../constants';
@@ -15,26 +15,24 @@ import {
 
 const SetRoute = ({ navigation }) => {
   const [distance, setDistance] = useState(0);
-  const [position, setPosition] = useState([]);
+  const [location, setLocation] = useState(null);
 
   useEffect(() => {
-    if (hasLocationPermission) {
-      Geolocation.getCurrentPosition(
-        (position) => {
-          setPosition(position);
-        },
-        (error) => {
-          throw error;
-        },
-        { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
-      );
-    }
+    Geolocation.getCurrentPosition(
+      (position) => {
+        const location = JSON.stringify(position);
+        setLocation(location);
+      },
+      (error) => Alert.alert(error.message),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    );
   });
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: theme.color.dark }}>
       <Container>
         <ScreenHeading>Set route</ScreenHeading>
+        <ScreenText>Current position: {location}</ScreenText>
         <ScreenText large bold style={{ textAlign: 'center' }}>
           How many km?
         </ScreenText>
